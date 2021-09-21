8c7d0b7c16a43b564925d8a6adf156b8fb99e712
Karolina <47788618+karolinakowalczyk@users.noreply.github.com>
2021-09-21 14:54:45 +0200

    no plan when no reservation

--- app/controllers/plans.controller.js
+++ app/controllers/plans.controller.js
@@ -7,10 +7,9 @@ exports.createPlan = async (req, res) => {
   const transport = req.body.transport;
   const attractions = req.body.attractions;
 
-    console.log(owner);
-    console.log(reservation);
-    console.log(transport);
-    console.log(attractions);
+  if (reservation.length === 0) {
+    return res.status(404).send({ message: "You cannot create a plan without reservation!" });
+  }
   const plan = new Plan({ owner: owner, reservation: reservation, transport: transport, attractions: attractions});
   await plan.save();
   
