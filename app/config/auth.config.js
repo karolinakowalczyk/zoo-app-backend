6516112ef65988c3c72f78c37fa2697f554fbb1c
Karolina Kowalczyk <k.kowalczyk@brainforge.pl>
2021-10-09 15:55:35 +0200

    small changes

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   secret: "zoo-app-key",
-  //jwtExpiration: 3600,           // 1 hour
-  //jwtRefreshExpiration: 86400,   // 24 hours
-
-  /* for test */
-  jwtExpiration: 60,          // 1 minute
-  jwtRefreshExpiration: 120,  // 2 minutes
+  jwtExpiration: 3600,           // 1 hour
+  jwtRefreshExpiration: 86400,   // 24 hours
 };
